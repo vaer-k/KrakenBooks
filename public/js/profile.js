94b3c8fec5c119dd71963f477d668507dc86162f
ying-hung Lai <yinghunglai@ying-hungs-MacBook-Air.local>
2015-07-29 11:55:35 -0700

    [feat]use only isbn number to list book for sell

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,38 +2,49 @@ angular.module('omnibooks.profile', ['ui.bootstrap','ngFileUpload','xeditable'])
 .run(function(editableOptions) {
   editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
 })
-.controller('ProfileController', ['$scope', '$stateParams', '$modal', '$state', 'auth', 'fireBase','Upload','$http',
-  function($scope, $stateParams, $modal, $state, auth, fireBase, Upload,$http) {
+.controller('ProfileController', ['$scope', '$stateParams', '$modal', '$state', 'auth', 'fireBase', 'bookinfoAPI', 'Upload','$http',
+  function($scope, $stateParams, $modal, $state, auth, fireBase, bookinfoAPI, Upload, $http) {
     var currentOrg = auth.getOrg();
     var currentUser = auth.getUser();
       $scope.upload = function (files) {
         if(files){
-          console.log('up load file!!!');
+          console.log('up load file!!!')
         console.log(files);
         var file = files[0];
       }
     };
 
-    $scope.enterBook = function(title, url, author, isbn, price, files) {
+    $scope.enterBook = function(url, isbn, price, files) {
       $scope.upload(files);
-      if (title && url && author && isbn) {
+      url = url||'http://books.sorodesign.com/wp-content/uploads/2010/01/book-cover-full-small.jpg'
+      if (isbn && price) {
         $scope.error = false;
         if (isbn.charAt(3) === '-') {
-          isbn = isbn.slice(0, 3) + isbn.slice(4);
-          console.log(isbn);
+          isbn = isbn.slice(0, 3) + isbn.slice(4)
+          console.log(isbn)
         }
-
-        if (price.charAt(0) === '$') {
-          price = price.slice(1);
-          console.log(price);
-        }
+        var displayDetail = function(res) {
+           // $scope.prices = res.data.data;
+          title = res[0].title_long;
+          author = res[0].author_data[0].name;
 
         fireBase.enterBook(currentOrg, currentUser.$id, title, url, author, isbn, price);
         console.log('successfully entered');
+           
+        };
+
+        if (price.charAt(0) === '$') {
+          price = price.slice(1);
+          console.log(price)
+        }
+
+        bookinfoAPI.getInfo(isbn, displayDetail);
+
       } else {
         $scope.error = "*You must fill out all required fields";
       }
-    };
+  };
+
 
     $scope.enterItem = function(itemImgUrl, itemName, itemDescription, itemPrice) {
       if (itemName && itemImgUrl && itemDescription) {
@@ -57,6 +68,7 @@ angular.module('omnibooks.profile', ['ui.bootstrap','ngFileUpload','xeditable'])
     $scope.org = auth.getOrg();
     $scope.noBooks = false;
     $scope.books = fireBase.getUserBookshelf($scope.org, $scope.username);
+
 
     if($scope.books.length === 0) {
       noBooks = true;
@@ -90,9 +102,9 @@ angular.module('omnibooks.profile', ['ui.bootstrap','ngFileUpload','xeditable'])
       img: $scope.bookEdit.img,
       isbn: $scope.bookEdit.isbn,
       askingPrice: $scope.bookEdit.askingPrice
-    };
+    }
     fireBase.updateBook($scope.org, $scope.username, $scope.bookEdit.$id, update);
-  };
+  }
 }])
 .directive('modal', function() {
   return {
@@ -114,4 +126,22 @@ angular.module('omnibooks.profile', ['ui.bootstrap','ngFileUpload','xeditable'])
       };
     }
   };
+})
+.factory('bookinfoAPI', function($http) {
+    var key = 'UTUJEB5A';
+    var getInfo = function(isbn, callback) {
+      return $http({
+          method: 'GET',
+          url: '/bookInfo',
+          params: {
+            'book_isbn': isbn
+          }
+        })
+        .then(function(res) {
+          callback(res.data.data);
+        });
+    };
+    return {
+      getInfo: getInfo
+    };
 });
