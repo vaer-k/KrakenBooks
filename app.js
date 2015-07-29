5a5dccb235a4f662a1470f05935d2b98bc23c9f6
Vincent Rupp Jr <vincent@vaerek.net>
2015-07-28 19:06:15 -0700

    [fix] correct call to render error page on server error handler

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
-// app.use('/', routes);
 app.use('/bookDetail', bookDetail);
 app.use('/sendMail', sendMail);
 
@@ -33,32 +32,7 @@ app.use('/sendMail', sendMail);
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
-  // next(err);
+  res.send(err);
 });
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-// if (app.get('env') === 'development') {
-//   app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//       message: err.message,
-//       error: err
-//     });
-//   });
-// }
-
-// production error handler
-// no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.render('error', {
-//     message: err.message,
-//     error: {}
-//   });
-// });
-
 
 module.exports = app;
